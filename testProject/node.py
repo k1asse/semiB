ec5cefc244dd0527c9c8cbcaf2c3baa043d9c616
k1asse <k1asse@localhost>
2020-01-06 12:18:56 +0900

    ユーザ・ノード間のネットワークを(だいたい)構成した 取引情報として送る文字列の先頭を"transaction"にすると 型だけ用意している(中身はまだ記述してない)関数が読み出される

--- testProject/node.py
+++ testProject/node.py
@@ -1,43 +1,140 @@
 from transaction import Transaction
-# ノード側のマシンが実行するプログラム
+import socket
+import selectors
+import sys
+
+sel = selectors.DefaultSelector()
+args = sys.argv
+NUMBER_OF_ZERO_SEQUENCE = 4
+
+if len(args) != 3:
+    print("usage: node.py [node port] [user port]")
+    sys.exit()
 
 
-class Node:
-    """ブロックの生成・検査を行うノード"""
+def check_transaction(str):
+    """
+    取引情報のチェックを行う
+    チェックに通らなかったら例外CheckTransactionErrorを呼び出す
+    """
+    print("取引情報がユーザからきました\n")
+    sender_name = ""
+    receiver_name = ""
+    value = 0
+    transaction = Transaction(sender_name, receiver_name, value)
+    return transaction
 
-    def __init__(self, user_port, node_port):
-        self.number = 1  # ノード番号
-        self.block_chain = []  # ブロックチェーン
-        self.user_port = user_port  # ユーザとの接続を行うポート
-        self.node_port = node_port  # ノードとの接続を行うポート
-        NUMBER_OF_ZERO_SEQUENCE = 4  # ブロック生成の権限に必要なハッシュ値の先頭0の個数
 
-    def connect_nodes(self):
-        """
-        すでに存在するノード群に接続する
-        """
+def assign_nonce(transaction):
+    """
+    ナンスを代入する、0が先頭にNUMBER_OF_ZERO_SEQUENCE個来たらノード全体に送信
+    """
+    print("ナンスを代入します\n")
+    # ナンスができたらそれをノード全体に送信する
 
-    def receive_transaction(self):
-        """
-        取引情報の受信を行う
-        """
 
-    def check_transaction(self):
-        """
-        取引情報のチェックを行う
-        """
 
-    def assign_nonce(self):
-        """
-        ナンスを代入する、0が先頭にNUMBER_OF_ZERO_SEQUENCE個来たらノード全体に送信
-        """
+def generate_block(transaction):
+    """
+    ブロックを生成する
+    """
+    print("ブロックを生成します\n")
 
-    def generate_block(self):
-        """
-        ブロックを生成する
-        """
 
-    def send_result(self, user, message):
-        """
-        ユーザに処理結果がOK/NGであったことを送信する
-        """
+def send_result(transaction, conn):
+    """
+    ユーザに処理結果がOK/NGであったことを送信する
+    """
+    print("ユーザに処理結果を送信します")
+    conn.send(b'process result from node')
+
+
+
+def accept_node(sock, mask):
+    conn, addr = sock.accept()
+    print('accepted', conn, 'from', addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read_node)
+
+
+def accept_user(sock, mask):
+    conn, addr = sock.accept()
+    print('accepted', conn, 'from', addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read_user)
+
+
+def read_node(conn, mask):
+    data = conn.recv(1000)
+    # ここの分岐をconn, dataによって行う
+    if data:
+        print('echoing', repr(data), 'to', conn)
+        conn.send(data)
+    else:
+        print('closing', conn)
+        sel.unregister(conn)
+        conn.close()
+
+
+def read_user(conn, mask):
+    data = conn.recv(1000)
+    # ここの分岐をconn, dataによって行う
+    if data:
+        recv_str = data.decode()
+        if recv_str.startswith('transaction'):
+            # 文字列のチェック・
+            try:
+                transaction = check_transaction(recv_str)
+            except CheckTransactionError as e:
+                print(e)
+            # 以下、マイニング作業を行う
+            # 適当に並べてるだけなので実際はtry-exceptとか関数とか変えていいと思う
+            # ナンスの代入(TODO ここソケット監視と並行に処理しないと無理かも(threadingとか))
+            assign_nonce(transaction)
+            # ブロックの生成
+            generate_block(transaction)
+            # 処理結果の送信
+            send_result(transaction, conn)
+            print("正しく取引を処理しました")
+        elif len(recv_str) == 0:   # ユーザからEOFが送られてきた場合
+            conn.close()
+        else:
+            print('echoing', repr(data), 'to', conn)
+            conn.send(data)
+    else:
+        print('closing', conn)
+        sel.unregister(conn)
+        conn.close()
+
+
+class CheckTransactionError(Exception):
+    """送られたtransactionに関するエラーを知らせる例外クラス"""
+    pass
+
+
+# ノード用のソケット
+sock1 = socket.socket()
+sock1.bind(('localhost', int(args[1])))
+sock1.listen(100)
+sock1.setblocking(False)
+sel.register(sock1, selectors.EVENT_READ, accept_node)
+
+# ユーザ用のソケット
+sock2 = socket.socket()
+sock2.bind(('localhost', int(args[2])))
+sock2.listen(100)
+sock2.setblocking(False)
+sel.register(sock2, selectors.EVENT_READ, accept_user)
+
+try:
+    while True:
+        # ノード用のソケットを登録
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            print(key.data)
+            callback(key.fileobj, mask)
+except KeyboardInterrupt:
+    print("interrupted.")
+    sock1.close()
+    sock2.close()
--- testProject/user.py
+++ testProject/user.py
@@ -1,4 +1,5 @@
 from transaction import Transaction
+import socket
 # ユーザ側のマシンが実行するプログラム
 
 
@@ -7,28 +8,45 @@ class User:
 
     def __init__(self, name):  # インスタンス作成時に呼び出されるメソッド
         self.name = name
-        self.__test_name = name
+        self.__test_name = name  # プライベート変数
         self.balance = 100
         self.friends = []
         print("user instance initialized name: " + self.name)
 
-    def send_request(self, transaction):
+    def send_request(self, transaction, port):
         """
         ユーザがノードたちに接続して、
         用意した取引情報を送信する
         """
         print("called send_request from " + self.name)
         transaction.print_short_data()
-        self.connect_nodes()  # ノードに接続する
+        self.connect_nodes(port)  # ノードに接続する
 
-    def connect_nodes(self):
+    def connect_nodes(self, port):
         """
         ノードたちに接続する (TODO send_requestに内包したらいいかも)
+        ユーザとの接続用port: 10000, 10001, ..., 10009
+        TCPな
         """
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # ノードを指定
+            s.connect(('127.0.0.1', port))
+            while True:
+                # 文字列を取得
+                string = input("string:").encode()
+                # サーバにメッセージを送る
+                s.sendall(string)
+                # ネットワークのバッファサイズは1024なので、サーバからの文字列取得も1024
+                data = s.recv(1024)
+                # サーバからくる文字列を表示
+                print(repr(data))
+
 
     def make_transaction(self):
         """
         取引情報を作成する
+        取引情報の文字列の先頭が"transaction"であれば
+        ノードが取引情報だと思ってくれます
         """
 
 
@@ -36,7 +54,7 @@ user = User("Takahashi")
 user2 = User("Suzuki")
 
 trans = Transaction("Takahashi", "Suzuki", 300)
-print(user.name)  # pythonは__を変数名の前につけないとpublic変数扱い
+print(user.name)  # pythonは_# _を変数名の前につけないとpublic変数扱い
 # print(user.__test_name) private変数化(厳密には違うらしい)されてるのでエラーが出る
-
-user.send_request(trans)
+port = int(input("Port Number: "))
+user.send_request(trans, port)
